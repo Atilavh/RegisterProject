6509145e9aedc06863dd289b6640eb04bdbfb6d1
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-28 16:30:26 +0330

    Create Users model for forms

--- AccountsApp/models.py
+++ AccountsApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class Users(AbstractUser):
+    phone = models.CharField(max_length=11, unique=True, verbose_name='شماره تلفن همراه')
+    confirm_code = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'کابر'
+        verbose_name_plural = 'کاربران'
+
+    def __str__(self):
+        return f'{self.fullname} {self.email}'
