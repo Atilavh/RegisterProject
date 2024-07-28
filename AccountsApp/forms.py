bd73d4eeb43eb652ce573da9b169a4ee6d47e498
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-28 16:29:42 +0330

    Update forms.py and Edit fields

--- AccountsApp/forms.py
+++ AccountsApp/forms.py
@@ -7,47 +7,77 @@ class RegisterForm(forms.Form):
         required=True,
         label='NickName',
         max_length=50,
+        error_messages={
+            'required': 'Please enter your full name',
+            'max_length': 'Please enter smaller than 50 lengh',
+        },
+        widget=forms.TextInput(attrs={
+            'placeholder': 'NickName',
+            'class': 'form__input',
+        })
     )
     phone_number = forms.CharField(
         required=True,
         label='PhoneNumber',
-        max_length=11
+        max_length=11,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'PhoneNumber',
+            'class': 'form__input',
+        })
     )
     email = forms.EmailField(
         required=True,
         label='EmailAddress',
-
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Email',
+            'class': 'form__input',
+        })
     )
     password = forms.CharField(
         required=True,
         label='Password',
         min_length=8,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form__input',
+        })
     )
     confirm_password = forms.CharField(
         required=True,
         label='ConfirmPassword',
         min_length=8,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'ConfirmPassword',
+            'class': 'form__input',
+        })
     )
 
-    def clean_confirm_password(self):
-        password = self.cleaned_data.get('password')
-        confirm_password = self.cleaned_data.get('confirm_password')
-        if password == confirm_password:
-            return confirm_password
-
-        raise ValidationError('کلمه عبور و تکرار کلمه عبور باهم مغایرت دارد')
+    # def clean_confirm_password(self):
+    #     password = self.cleaned_data.get('password')
+    #     confirm_password = self.cleaned_data.get('confirm_password')
+    #     if password == confirm_password:
+    #         return confirm_password
+    #
+    #     raise ValidationError('کلمه عبور و تکرار کلمه عبور باهم مغایرت دارد')
 
 
 class LoginForm(forms.Form):
     email = forms.EmailField(
         required=True,
         label='EmailAddress',
-
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Email',
+            'class': 'form__input',
+        })
     )
     password = forms.CharField(
         required=True,
         label='Password',
         min_length=8,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Password',
+            'class': 'form__input',
+        })
     )
 
 
